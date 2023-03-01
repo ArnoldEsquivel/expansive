9c69424f2c9a40ca6b41265e680b7add31c7e9ff
Arnold Esquivel <arnold_526@outlook.com>
2023-02-28 20:27:40 -0600

    ADD - Cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const connectionDB = require('./mongo.js');
+const cors = require('cors');
 
 // Importación de rutas
 const ubicaciones = require('./serverCarp/routes/ubicaciones.js');
 
+app.use(cors({origin: 'http://164.92.178.206'}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
