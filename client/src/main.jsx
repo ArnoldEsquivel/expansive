c08a682e8d3556c96c254487abdf54ca0cab8c2f
Arnold Esquivel <arnold_526@outlook.com>
2023-02-28 21:47:55 -0600

    prueba

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://164.92.178.206:80/api'
+axios.defaults.baseURL = 'http://localhost:3000/api'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const cors = require('cors');
 // Importación de rutas
 const ubicaciones = require('./serverCarp/routes/ubicaciones.js');
 
-app.use(cors({origin: 'http://164.92.178.206'}));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+// app.use(cors({origin: 'http://http://127.0.0.1:5173'}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
 
-//     next();
-// });
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
