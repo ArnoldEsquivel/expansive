b37ebe569fc1dfc2bfa5dd8623ab204574e71692
Arnold Esquivel <arnold_526@outlook.com>
2023-03-02 21:30:10 -0600

    ADD - port

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://164.92.178.206:3000/api'
+axios.defaults.baseURL = 'http://164.92.178.206:5000/api'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
