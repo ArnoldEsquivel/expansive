6b3d518cfcc3d05e419ac3926add704b69dd5a67
Arnold Esquivel <arnold_526@outlook.com>
2023-03-02 21:17:25 -0600

    ADD - dist

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // Importación de rutas
 const ubicaciones = require('./serverCarp/routes/ubicaciones.js');
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors({ origin: 'http://http://164.92.178.206:5173' }));
 // app.use((req, res, next) => {
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', ubicaciones);
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, "0.0.0.0", () => {
