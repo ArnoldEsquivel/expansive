3101533061c9c3da20b806377d1fbdef6ab7455c
Arnold Esquivel <arnold_526@outlook.com>
2023-03-02 20:57:31 -0600

    ADD - mod to prod

--- server.js
+++ server.js
@@ -3,24 +3,32 @@ const bodyParser = require('body-parser');
 const app = express();
 const connectionDB = require('./mongo.js');
 const cors = require('cors');
+const path = require('path');
+const port = process.env.PORT || 5000;
 
 // Importación de rutas
 const ubicaciones = require('./serverCarp/routes/ubicaciones.js');
 
-// app.use(cors({origin: 'http://http://127.0.0.1:5173'}));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+app.use(express.static(path.join(__dirname, 'build')));
 
-    next();
-});
+app.use(cors({ origin: 'http://http://164.92.178.206:5173' }));
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+//     next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', ubicaciones);
 
-app.listen(3000, () => {
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, "0.0.0.0", () => {
     console.log('Servidor iniciado en el puerto 3000');
 });
