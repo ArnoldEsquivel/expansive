eb9846bb474ddb06c3a27c6e17102414823a8ab8
Arnold Esquivel <arnold_526@outlook.com>
2023-03-15 12:11:59 -0600

    DIST - Estilado mobile

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'http://164.92.178.206:5000/api'
-axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.baseURL = 'http://164.92.178.206:5000/api'
+// axios.defaults.baseURL = 'http://localhost:5000/api'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
